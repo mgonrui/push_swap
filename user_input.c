1f36563625694a8bbcb0d4320eaf09429e950e39
deckard <mgonrui>
2025-04-18 14:28:31 +0200

    fix user input errors

--- user_input.c
+++ user_input.c
@@ -66,9 +66,12 @@ bool valid_number(char *str)
     int i;
     i = 0;
     if (str[0] == '-' || str[0] == '+')
+    {
+        if (str[1] == '\0')
+            return false;
+    }
+    else
         i++;
-    if ((str[0] == '-' || str[0] == '+') && str[1] == '\0')
-        return false;
     while (str[i] != '\0')
     {
         if (!ft_isdigit(str[i]))
